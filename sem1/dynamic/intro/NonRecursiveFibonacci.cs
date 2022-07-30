019d2834c32d21b17d1044fdd20ef48021e7dbbb
ShurkaWake <92213591+ShurkaWake@users.noreply.github.com>
2022-07-30 11:06:22 +0300

    Added non-recursive fibonacci in C#

---
+++ sem1/dynamic/intro/NonRecursiveFibonacci.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace algo
+{
+    public class Fibonacci
+    {
+        public static int fib(int n)
+        {
+            if (n < 0)
+            {
+                throw new ArgumentException();
+            }
+            
+            if (n <= 1)
+            {
+                return n;
+            }
+
+            int prelast = 0;
+            int last = 1;
+
+            for (int i = 2; i <= n; i++)
+            {
+                int nextFib = prelast + last;
+                prelast = last;
+                last = nextFib;
+            }
+
+            return last;
+        }
+    }
+}
