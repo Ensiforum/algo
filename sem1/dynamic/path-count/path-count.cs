afb44ef8abdea1b22e76536eda2e05f73330012e
ShurkaWake <92213591+ShurkaWake@users.noreply.github.com>
2022-07-30 13:17:48 +0300

    Added path count in C#

---
+++ sem1/dynamic/path-count/path-count.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace algo
+{
+    public class path_count
+    {
+        public static int GetPathCount(int[,] adj, int s, int t, int k)
+        {
+            int[,] matrix = PowMatrix(adj, k);
+            return matrix[s, t];
+        }
+        
+        private static int[,] MultiplyMatrices(int[,] first, int[,] second)
+        {
+            if (first.GetLength(1) != second.GetLength(0))
+            {
+                throw new ArgumentException("Matrices cannot be multiplied");
+            }
+
+            int[,] result = new int[first.GetLength(0), second.GetLength(1)];
+
+            for (int i = 0; i < first.GetLength(0); i++)
+            {
+                for (int j = 0; j < second.GetLength(1); j++)
+                {
+                    for (int k = 0; k < first.GetLength(1); k++)
+                    {
+                        result[i, j] += first[i, k] * second[k, j];
+                    }
+                }
+            }
+
+            return result;
+        }
+        
+        private static int[,] PowMatrix(int[,] matrix, int pow)
+        {
+            if (matrix.GetLength(0) != matrix.GetLength(1))
+            {
+                throw new ArgumentException("Matrix cannot be powered");
+            }
+            
+            if (pow == 1)
+            {
+                return matrix;
+            }
+
+            if (pow % 2 == 0)
+            {
+                var temp = PowMatrix(matrix, pow / 2);
+                return MultiplyMatrices(temp, temp);
+            }
+            return MultiplyMatrices(PowMatrix(matrix, pow - 1), matrix);
+        }
+    }
+}
